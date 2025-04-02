0e59d1957ee0f65e22d4852cf149c142107c326d
gharrowbm <gharrow.games@gmail.com>
2025-04-02 20:23:21 +0200

    Add project files.

---
+++ Extensions/MyExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1.Extensions
+{
+    public static class MyExtensions
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            var dbHostname = Environment.GetEnvironmentVariable("DB_HOSTNAME");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+            var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            var connectionString = $"Server=tcp:{dbHostname},1433;Initial Catalog={dbName};Persist Security Info=False;User ID={dbUser};Password={dbPassword};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+        }
+    }
+}
